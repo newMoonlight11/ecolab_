228c7f7a93d34629a4ec08f1dde642896ad43a03
Cescobar332 <cescobar332@unab.edu.co>
2024-07-04 20:15:26 -0500

    Manifest

--- config/app.php
+++ config/app.php
@@ -123,9 +123,9 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'providers' => [
+    /*'providers' => [
     // ...
         Spatie\Permission\PermissionServiceProvider::class,
-    ],
+    ],*/
 
 ];
