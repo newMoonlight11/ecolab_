716e3f744312ea96a48dfd86413361c9fdbf10d7
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2024-07-11 20:03:47 -0500

    login 2.0

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3 card_inside">
+                        <div class="row mb-3">
                             <div class="col-md-8 offset-md-2">
                                 <p class="text-center">Bienvenido de nuevo!</p>
                                 <input id="email" type="email"
@@ -32,7 +32,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3 card_inside">
+                        <div class="row mb-3">
                             <div class="col-md-8 offset-md-2">
                                 <input id="password" type="password"
                                     class="form-control @error('password') is-invalid @enderror" name="password"
@@ -47,24 +47,22 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-8 offset-md-2">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Recordarme') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
                             <div class="col-md-8 offset-md-2 text-center">
+                                {{-- <br> --}}
                                 <button type="submit" class="button_login">
                                     {{ __('INICIAR SESIÓN') }}
                                 </button>
-                                <br>
+                            </div>
+                            <div class="col-md-8 offset-md-2 d-flex justify-content-between align-items-center">
+                                {{-- <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
+                        
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Recordarme') }}
+                                    </label>
+                                </div> --}}
+                                <div class="linea flex-grow-1"></div>
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Olvidaste tu contraseña?') }}
@@ -74,9 +72,9 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-8 offset-md-2 text-center">
+                            <div class="col-md-8 offset-md-2 d-flex justify-content-between align-items-center">
                                 <p>¿No tienes cuenta?</p>
-                                <p>Regístrate</p>
+                                <a class="btn btn-link" href="{{ route('register') }}">{{ __('Regístrate') }}</a>
                             </div>
                         </div>
                     </form>
