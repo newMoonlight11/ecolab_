259f49678a16aeacc6049065d399e72385eaef25
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2024-08-29 11:03:29 -0500

    migraciones laboratorio

--- resources/views/reactivo/index.blade.php
+++ resources/views/reactivo/index.blade.php
@@ -96,10 +96,13 @@
                                                 <td class="col-sm-1">
                                                     <form action="{{ route('reactivos.destroy', $reactivo->id) }}"
                                                         method="POST" class="d-inline">
-                                                        <a class="btn btn-sm"
+                                                        {{-- <a class="btn btn-sm"
                                                             href="{{ route('reactivos.show', $reactivo->id) }}">
                                                             <i class="bi bi-search text-primary fs-5"></i>
-                                                        </a>
+                                                        </a> --}}
+                                                        <button type="button" class="btn btn-link" data-bs-toggle="modal"
+                                                            data-bs-target="#termsModal"><i
+                                                                class="bi bi-search text-primary fs-5"></i></button>
                                                         <a class="btn btn-sm"
                                                             href="{{ route('reactivos.edit', $reactivo->id) }}">
                                                             <i class="bi bi-pencil text-pop fs-5"></i>
