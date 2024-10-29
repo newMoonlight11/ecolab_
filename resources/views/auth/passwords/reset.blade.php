2980c843e6114719fb2df5a12e42635ff17f3022
Cescobar332 <cescobar332@unab.edu.co>
2024-10-28 20:41:23 -0500

    Poner bonito el Olvidaste tu contraseña?

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,56 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card bg-white border-0 rounded-4">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card bg-white border-0 rounded-4">
+                    <div class="card-body">
                         <div class="row mb-3">
                             <div class="col-md-8 offset-md-2 text-center">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus placeholder="{{ __('Correo electrónico') }}">
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <p class="color-blue">Restablecer contraseña</p>
                             </div>
                         </div>
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="{{ __('Contraseña') }}">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="row mb-3">
+                                <div class="col-md-8 offset-md-2">
+                                    <input id="email" type="email"
+                                        class="form-control bg-white rounded-4 @error('email') is-invalid @enderror"
+                                        name="email" value="{{ $email ?? old('email') }}" required autocomplete="email"
+                                        autofocus placeholder="{{ __('Correo electrónico') }}">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="{{ __('Confirma tu contraseña') }}">
+                            <div class="row mb-3">
+                                <div class="col-md-8 offset-md-2">
+                                    <input id="password" type="password"
+                                        class="form-control bg-white rounded-4 @error('password') is-invalid @enderror"
+                                        name="password" required autocomplete="new-password"
+                                        placeholder="{{ __('Nueva Contraseña') }}">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Cambiar contraseña') }}
-                                </button>
+                            <div class="row mb-3">
+                                <div class="col-md-8 offset-md-2">
+                                    <input id="password-confirm" type="password" class="form-control bg-white rounded-4"
+                                        name="password_confirmation" required autocomplete="new-password"
+                                        placeholder="{{ __('Confirmar Nueva Contraseña') }}">
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="row mb-3">
+                                <div class="col-md-8 offset-md-2 text-center">
+                                    <button type="submit" class="btn btn-primary rounded-4 w-75">
+                                        {{ __('Restablecer Contraseña') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
